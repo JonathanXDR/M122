a64b89a9c60886bd47896f9f6ff90bdcdd8ef738
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2022-07-07 11:42:49 +0200

    Update Script1

--- bin/Script_1/git_clone_update_repos.bash
+++ bin/Script_1/git_clone_update_repos.bash
@@ -1,67 +1,80 @@
 #!/bin/bash
-# save the current working directory in $cwd
-cwd=`pwd`
-# change to the directory where the script is located
-cd `dirname $0`
-# save in $BINDIR the directory where the script is 
-BINDIR=`pwd`
-# return to the working directory	
-cd $cwd
-# Set the script name (without path to it)
-BASENAME=`basename $0`
-# Set a temporary directory if needed
-TMPDIR=/tmp/$BASENAME.$$
-# $ETCDIR is the config directory
-ETCDIR=$BINDIR/../etc
+cwd=`pwd`		# save the current working directory in $cwd
+cd `dirname $0`	# change to the directory where the script is located
+BINDIR=`pwd`	# save in $BINDIR the directory where the script is 
+cd $cwd		# return to the working directory
+BASENAME=`basename $0`	# Set the script name (without path to it)
+TMPDIR=/tmp/$BASENAME.$$	# Set a temporary directory if needed
+ETCDIR=$BINDIR/../etc		# $ETCDIR is the config directory
 
 # Read input file, which contains the git urls and the directories to clone to in the format:
 # <git url> <directory to clone to>
 
-# Params: $1 - input file with the git urls and the directories to clone to
-#         $2 - local base directory to clone to
-
-# variables for the parameters
 input_file=$1
-local_base_dir=$2
+base_dir=$2
 
-# Base directory exists? If not, create it.
-if [ ! -d $local_base_dir ]; then
-  mkdir -p $local_base_dir
-fi
-
-# check if the input file exists and is readable
-if [ ! -r $input_file ]; then
-  echo "Error: input file $input_file does not exist or is not readable"
+# if parameters are not set, exit
+if [ -z "$input_file" ] || [ -z "$base_dir" ]; then
+  echo "Error: parameters are not set"
+  echo "Usage: $BASENAME <input file> <target directory>"
   exit 1
 fi
 
-# git repo from input file exists in the local base directory? If not, clone it. If yes, update it.
-while read line; do
+# Base directory exists? If not, create it.
+if [ ! -d $base_dir ]; then
+  mkdir -p $base_dir
+fi
+
+# check if the input file exists
+if [ ! -f "$input_file" ] ; then
+  echo "Error: $input_file does not exist"
+  exit 3
+fi
+
+# check if input file is empty
+if [ ! -s "$input_file" ] ; then
+  echo "Error: $input_file is empty"
+  exit 4
+fi
+
+# git repo from input file exists in the base directory? If not, clone it. If yes, update it.
+while read line || [ -n "$line" ] ; 
+do
   # split the line into the git url and the directory to clone to
   git_url=`echo $line | cut -d' ' -f1`
-  local_dir=`echo $line | cut -d' ' -f2`
+  target_dir=`echo $line | cut -d' ' -f2`
   # check if the local directory exists
-  if [ ! -d $local_base_dir/$local_dir ]; then
-    echo "Cloning $git_url to $local_base_dir/$local_dir"
-    git clone $git_url $local_base_dir/$local_dir
+  if [ ! -d $base_dir/$target_dir ]; then
+    echo "Cloning $git_url to $base_dir/$target_dir"
+    git clone $git_url $base_dir/$target_dir
   else
-    echo "Updating $git_url in $local_base_dir/$local_dir"
-    cd $local_base_dir/$local_dir
+    echo "Updating $git_url in $base_dir/$target_dir"
+    cd $base_dir/$target_dir
     git pull
     cd $cwd
   fi
 done < $input_file
 
-# if directory in base directory exists, which is not in the input file, delete it.
-for dir in `ls -d $local_base_dir/*/`; do
-  dir=`echo $dir | sed 's/\/$//'`
-  if [ ! -r $input_file ]; then
-    echo "Deleting $dir"
-    rm -rf $dir
+# Remove all files or directories from the base directory, where the names are not contained in the input file
+for file in $base_dir/*; do
+  if [ ! -d $file ]; then
+    continue
+  fi
+  # get the name of the directory
+  dir_name=`basename $file`
+  # check if the directory name is contained in the input file
+  if grep -q $dir_name $input_file; then
+    continue
+  fi
+
+  # ask user if the directory should be removed and reuse the answer for all other directories
+  if [ -z "$answer" ]; then
+    echo "Removing all directories not contained in $input_file"
+    echo "Do you want to continue? (y/n)"
+    read answer
+  fi
+  if [ "$answer" == "y" ]; then
+    rm -rf $file
+    echo "Removing $dir_name"
   fi
 done
-
-# Next line in csv file exists? If yes, repeat the process. If no, exit.
-if [ -r $input_file ]; then
-  $0 $input_file $local_base_dir
-fi
