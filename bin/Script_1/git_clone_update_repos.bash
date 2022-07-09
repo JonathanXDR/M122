39c6ca6f05951f69bcefc53206b9ebabdd5f5275
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2022-07-09 13:34:26 +0200

    Update errors

--- bin/Script_1/git_clone_update_repos.bash
+++ bin/Script_1/git_clone_update_repos.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
-cwd=`pwd`		# save the current working directory in $cwd
-cd `dirname $0`	# change to the directory where the script is located
-BINDIR=`pwd`	# save in $BINDIR the directory where the script is 
-cd $cwd		# return to the working directory
-BASENAME=`basename $0`	# Set the script name (without path to it)
-TMPDIR=/tmp/$BASENAME.$$	# Set a temporary directory if needed
-ETCDIR=$BINDIR/../etc		# $ETCDIR is the config directory
+cwd=$(pwd)               # save the current working directory in $cwd
+cd $(dirname $0)         # change to the directory where the script is located
+BINDIR=$(pwd)            # save in $BINDIR the directory where the script is
+cd $cwd                  # return to the working directory
+BASENAME=$(basename $0)  # Set the script name (without path to it)
+TMPDIR=/tmp/$BASENAME.$$ # Set a temporary directory if needed
+ETCDIR=$BINDIR/../etc    # $ETCDIR is the config directory
 
 # Read input file, which contains the git urls and the directories to clone to in the format:
 # <git url> <directory to clone to>
@@ -26,34 +26,33 @@ if [ ! -d $base_dir ]; then
 fi
 
 # check if the input file exists
-if [ ! -f "$input_file" ] ; then
+if [ ! -f "$input_file" ]; then
   echo "Error: $input_file does not exist"
   exit 3
 fi
 
 # check if input file is empty
-if [ ! -s "$input_file" ] ; then
+if [ ! -s "$input_file" ]; then
   echo "Error: $input_file is empty"
   exit 4
 fi
 
 # git repo from input file exists in the base directory? If not, clone it. If yes, update it.
-while read line || [ -n "$line" ] ; 
-do
+while read line || [ -n "$line" ]; do
+  echo ""
   # split the line into the git url and the directory to clone to
-  git_url=`echo $line | cut -d' ' -f1`
-  target_dir=`echo $line | cut -d' ' -f2`
+  git_url=$(echo $line | cut -d' ' -f1)
+  target_dir=$(echo $line | cut -d' ' -f2)
   # check if the local directory exists
   if [ ! -d $base_dir/$target_dir ]; then
-    echo "Cloning $git_url to $base_dir/$target_dir"
     git clone $git_url $base_dir/$target_dir
   else
-    echo "Updating $git_url in $base_dir/$target_dir"
+    echo "Updating $base_dir/$target_dir..."
     cd $base_dir/$target_dir
     git pull
     cd $cwd
   fi
-done < $input_file
+done <$input_file
 
 # Remove all files or directories from the base directory, where the names are not contained in the input file
 for file in $base_dir/*; do
@@ -61,7 +60,7 @@ for file in $base_dir/*; do
     continue
   fi
   # get the name of the directory
-  dir_name=`basename $file`
+  dir_name=$(basename $file)
   # check if the directory name is contained in the input file
   if grep -q $dir_name $input_file; then
     continue
@@ -69,12 +68,13 @@ for file in $base_dir/*; do
 
   # ask user if the directory should be removed and reuse the answer for all other directories
   if [ -z "$answer" ]; then
-    echo "Removing all directories not contained in $input_file"
+    echo ""
+    echo "Removing all directories not contained in $input_file..."
     echo "Do you want to continue? (y/n)"
     read answer
   fi
   if [ "$answer" == "y" ]; then
     rm -rf $file
-    echo "Removing $dir_name"
+    echo "Removing $dir_name..."
   fi
 done
